6ceba1dd92a10ddc3b33389bda733da2f778cc44
Lucas Lopes <dev.thuram@gmail.com>
2021-08-05 20:06:45 -0300

    [LL] - create main with provider and s3 configs

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
-    
+  region = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_s3_bucket" "mob-prog" {
+  bucket = "demo-mob-prog-tw-only"
+  acl    = "private"
 }
