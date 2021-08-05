bf82cbefc9b6be8668de6d9dd814cd15a19e21a0
henrifalves <henri.falves@hotmail.com>
2021-08-05 19:31:51 -0300

    [HA] Adds variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+    default = "AKIA26Z3ODVN5JDH2RTM"
+}
+
+variable "aws_secret_key" {
+    default = "n+VOHXm9TSxYK4dpJ9g9iG2J6SsAq8RKefShBaCq"
+}
+
+variable "region" {
+    default = "us-east-2"
+}
